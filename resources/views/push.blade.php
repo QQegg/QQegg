b9f6704a9f5f2b8410b8a4c928a8c991e816c443
陳美妤\admin <s3a432090@student.ncut.edu.tw>
2018-01-05 20:12:07 +0800

    新增推播詳細內容的頁面

---
+++ resources/views/push.blade.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Sailor - Bootstrap 3 corporate template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Bootstrap 3 template for corporate business" />
+    <!-- css -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="plugins/flexslider/flexslider.css" rel="stylesheet" media="screen" />
+    <link href="css/cubeportfolio.min.css" rel="stylesheet" />
+    <link href="css/style.css" rel="stylesheet" />
+
+    <!-- Theme skin -->
+    <link id="t-colors" href="skins/default.css" rel="stylesheet" />
+
+    <!-- boxed bg -->
+    <link id="bodybg" href="bodybg/bg1.css" rel="stylesheet" type="text/css" />
+
+    <!-- =======================================================
+    Theme Name: Sailor
+    Theme URL: https://bootstrapmade.com/sailor-free-bootstrap-theme/
+    Author: BootstrapMade
+    Author URL: https://bootstrapmade.com
+    ======================================================= -->
+
+</head>
+
+<body>
+
+
+
+<!-- start header -->
+{{----}}
+<!-- end header -->
+
+
+<section id="content">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8">
+                <article>
+                    <div class="post-image">
+                        <div class="post-heading">
+                            <h3><a href="#">This is an example of standard post format</a></h3>
+                        </div>
+                        <p>
+                            Qui ut ceteros comprehensam. Cu eos sale sanctus eligendi, id ius elitr saperet, ocurreret pertinacia pri an. No mei nibh consectetuer, semper laoreet perfecto ad qui, est rebum nulla argumentum ei. Fierent adipisci iracundia est ei, usu timeam persius
+                            ea. Usu ea justo malis, pri quando everti electram ei, ex homero omittam salutatus sed.
+                        </p>
+                        <img src="img/dummies/blog/img1.jpg" alt="" class="img-responsive" />
+                    </div>
+
+                    <div class="bottom-article">
+                        <a href="#" class="  btn  btn-success"><B>加入收藏</B></a>
+                        <a href="#" class="readmore pull-right">查看店家 <i class="fa fa-angle-right"></i></a>
+                    </div>
+                </article>
+
+                <article>
+                    <div class="post-slider">
+                        <div class="post-heading">
+                            <h3><a href="#">This is an example of slider post format</a></h3>
+                        </div>
+                        <!-- start flexslider -->
+                        <div id="post-slider" class="postslider flexslider">
+                            <p>
+                                Qui ut ceteros comprehensam. Cu eos sale sanctus eligendi, id ius elitr saperet, ocurreret pertinacia pri an. No mei nibh consectetuer, semper laoreet perfecto ad qui, est rebum nulla argumentum ei. Fierent adipisci iracundia est ei, usu timeam persius
+                                ea. Usu ea justo malis, pri quando everti electram ei, ex homero omittam salutatus sed.
+                            </p>
+                            <ul class="slides">
+                                <li>
+                                    <img src="img/dummies/blog/img1.jpg" alt="" />
+                                </li>
+                                <li>
+                                    <img src="img/dummies/blog/img2.jpg" alt="" />
+                                </li>
+                                <li>
+                                    <img src="img/dummies/blog/img3.jpg" alt="" />
+                                </li>
+                            </ul>
+                        </div>
+                        <!-- end flexslider -->
+                    </div>
+
+                    <div class="bottom-article">
+                        <a href="#" class="  btn  btn-success"><B>加入收藏</B></a>
+                        <a href="#" class="readmore pull-right">查看店家 <i class="fa fa-angle-right"></i></a>
+                    </div>
+                </article>
+                <article>
+                    <div class="post-quote">
+                        <div class="post-heading">
+                            <h3><a href="#">Nice example of quote post format below</a></h3>
+                        </div>
+                        <blockquote>
+                            <i class="fa fa-quote-left"></i> Lorem ipsum dolor sit amet, ei quod constituto qui. Summo labores expetendis ad quo, lorem luptatum et vis. No qui vidisse signiferumque...
+                        </blockquote>
+                    </div>
+                    <div class="bottom-article">
+                        <a href="#" class="  btn  btn-success"><B>加入收藏</B></a>
+                        <a href="#" class="readmore pull-right">查看店家 <i class="fa fa-angle-right"></i></a>
+                    </div>
+                </article>
+
+                <a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
+                <!-- javascript
+                ================================================== -->
+                <!-- Placed at the end of the document so the pages load faster -->
+                <script src="js/jquery.min.js"></script>
+                <script src="js/modernizr.custom.js"></script>
+                <script src="js/jquery.easing.1.3.js"></script>
+                <script src="js/bootstrap.min.js"></script>
+                <script src="plugins/flexslider/jquery.flexslider-min.js"></script>
+                <script src="plugins/flexslider/flexslider.config.js"></script>
+                <script src="js/jquery.appear.js"></script>
+                <script src="js/stellar.js"></script>
+                <script src="js/classie.js"></script>
+                <script src="js/uisearch.js"></script>
+                <script src="js/jquery.cubeportfolio.min.js"></script>
+                <script src="js/google-code-prettify/prettify.js"></script>
+                <script src="js/animate.js"></script>
+                <script src="js/custom.js"></script>
+
+
+</body>
+
+</html>
