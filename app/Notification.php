8b4b29c4c90a84528e77768ce00aa5ba18378223
SKILL573 <a9101021249@gmail.com>
2018-01-17 14:43:40 +0800

    推播MODEL建立

--- app/Notification.php
+++ app/Notification.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Notification extends Model
 {
-    //
+    protected $table='pushs';
+    protected $fillable=['id','S_id','C_id','Cate_id','P_title','P_content','P_timestamp'];
 }
