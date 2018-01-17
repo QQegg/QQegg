41e5adeb1e548a8d1b3c53a8b666f2005177bab5
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2018-01-17 14:46:24 +0800

    修改model

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable=[
+        'id',
+        'Cate_id',
+        'Comm_name',
+        'Comm_spec',
+        'Comm_price',
+        'Comm_unit',
+        'Comm_inv',
+    ];
+
+    protected $table="commoditys";
 }
