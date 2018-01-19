dfe674e595ba32896fadc435e56252128d8a7d68
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2018-01-19 23:32:05 +0800

    增加刪除商品功能以及將修改商品功能補齊

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\DB;
 use App\Product;
 use Illuminate\Http\Request;
 
@@ -21,14 +21,21 @@ class ProductsController extends Controller
         Product::create($request->all());
         return redirect()->route('procreate');
     }
-    public function edit()
+    public function edit($id)
     {
+        $product=Product::all()->where('id',$id);
+        return view('product.productedit',compact('product'));
     }
     public function update(Request $request,$id)
     {
-
+        $product=Product::find($id);
+        $product->update($request->all());
+        return redirect()->route('prolist');
     }
-    public function destroy()
+    public function destroy($id)
     {
+        $whereArray = array('id'=>$id);
+        DB::table('commoditys')->where($whereArray)->delete();
+        return redirect()->route('prolist');
     }
 }
