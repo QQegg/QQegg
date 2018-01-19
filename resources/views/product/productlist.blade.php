3787efcf652679d9ecf1e6389626f4873197655c
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2018-01-19 23:35:08 +0800

    將刪除按鈕修改成form的型式，以及加上刪除前的javascript的確認功能

--- resources/views/product/productlist.blade.php
+++ resources/views/product/productlist.blade.php
@@ -1,6 +1,11 @@
 @extends('product.layout.master')
 @section('title','書籍觀看')
 @section('content')
+    @if(count($product) == 0)
+        <p class="text-center">
+            沒有任何商品
+        </p>
+    @endif
     @foreach($product as $product)
 <div class="panel panel-default">
     <div class="panel-body">
@@ -36,14 +41,24 @@
             </div>
             <div class="row" style="margin-top:10px;">
                 <div class="col-md-12">
-                    <a href="{{route('proupdate')}}" class="btn btn-xs btn-danger">產品修改</a>
+                    <a href="{{route('proedit',$product->id)}}" class="btn btn-xs btn-danger">產品修改</a>
                 </div>
             </div>
-            <div class="row" style="margin-top:10px;">
-                <div class="col-md-12">
-                    <a href="#" class="btn btn-xs btn-danger">產品下架</a>
-                </div>
-            </div>
+            <script>
+                function ConfirmDelete()
+                {
+                    var x = confirm("你確定要刪除此產品嗎?");
+                    if (x)
+                        return true;
+                    else
+                        return false;
+                }
+            </script>
+            <form class="delete" action="{{ route('prodestroy', $product->id) }}"method="POST" onsubmit="return ConfirmDelete()">
+                <input type="hidden" name="_method" value="DELETE">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+                <input type="submit"class="btn btn-xs btn-danger" value="產品下架">
+            </form>
         </div>
     </div>
 </div>
