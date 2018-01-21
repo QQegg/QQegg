11cc5eaf151974087d6f7a8455f4a077ed37a69d
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2018-01-22 00:33:36 +0800

    呈現圖片的程式碼(未完成)

--- resources/views/product/productlist.blade.php
+++ resources/views/product/productlist.blade.php
@@ -15,6 +15,7 @@
                     <h1 style="margin-top:0;"></h1>
                 </div>
             </div>
+            <img id="upmg" src="{{asset('upload/'.$product->Comm_img)}}">
             <div class="row">
                 <div class="col-md-12">
                     <font size=5>商品名稱:{{$product->Comm_name}}</font>
