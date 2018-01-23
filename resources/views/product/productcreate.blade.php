78b7b8971e2ce3b1b11cff37bf6b0f24117bb72a
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2018-01-23 15:50:10 +0800

    修改上傳圖片的view

--- resources/views/product/productcreate.blade.php
+++ resources/views/product/productcreate.blade.php
@@ -35,7 +35,7 @@
             </div>
             <div class="form-group">
                 <label>上傳產品照片</label>
-                    <input type="file" class="form-control" id="picture" name="Comm_img" placeholder="上傳圖片" value="">
+                    <input type="file" class="form-control" id="picture" name="Comm_img" placeholder="上傳圖片">
             </div>
             <div class="text-right">
                 <button type="submit" class="btn btn-success">新增</button>
