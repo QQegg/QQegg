c0a34e46c4255b5349d2e22a9202ce8b83156b55
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2018-02-05 15:36:45 +0800

    更新產品資料的驗證方法

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -69,6 +69,31 @@ class ProductsController extends Controller
     }
     public function update(Request $request,$id)
     {
+        $messsages = array(
+            'name.required'=>'你必須輸入產品名稱',
+            'specification.required'=>'你必須輸入產品規格',
+            'price.required'=>'你必須輸入單價',
+            'unit.required'=>'你必須輸入單位',
+            'inventory.required'=>'你必須輸入庫存量',
+            'picture.required'=>'你必須選擇照片',
+        );
+
+        $rules = array(
+            'name' => 'required|max:255',
+            'Category_id' => 'required',
+            'specification' => 'required',
+            'price' => 'required|integer',
+            'unit' => 'required|string',
+            'inventory' => 'required|integer',
+            'picture' => 'required',
+        );
+
+        $validator = Validator::make($request->all(), $rules,$messsages);
+
+        if ($validator->fails())
+        {
+            return redirect()->back()->withErrors($validator->errors());
+        }
         $product=Product::find($id);
         $product->update($request->all());
         if ($request->hasFile('picture')){
