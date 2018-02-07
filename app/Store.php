73a2c4c9a331d06c5dca6ea76949927aa8fd1680
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2018-02-07 18:37:37 +0800

    將商家model連上資料庫

--- app/Store.php
+++ app/Store.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Store extends Model
 {
-    //
+    protected $fillable = [
+        'id',
+        'name',
+        'contact',
+        'account',
+        'password',
+        'phone',
+        'address',
+        'picture',
+        'right',
+        'expire',
+    ];
+
+    protected $table="stores";
 }
