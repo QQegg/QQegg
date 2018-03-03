4d940d780dc8806bcf789aef3be97ed29861a6cd
SKILL573 <a9101021249@gmail.com>
2018-03-03 11:58:28 +0800

    登入頁面

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,12 +10,12 @@ class HomeController extends Controller
      * Create a new controller instance.
      *
      * @return void
-
+     */
     public function __construct()
     {
         $this->middleware('auth');
     }
-     */
+
     /**
      * Show the application dashboard.
      *
@@ -23,6 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('no use.index');
+        return view('home');
     }
 }
