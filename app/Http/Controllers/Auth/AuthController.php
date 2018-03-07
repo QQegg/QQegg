327bf328566cebf027d48145d825d5631eea48b6
SKILL573 <a9101021249@gmail.com>
2018-03-07 18:51:24 +0800

    修改登入的驗證規則

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -43,7 +43,7 @@ class AuthController extends Controller
     {
         return Validator::make($data, [
             'name' => 'required|max:255',
-            'email' => 'required|email|max:255|unique:users',
+            'account' => 'required|max:255|unique:users',
             'password' => 'required|confirmed|min:6',
         ]);
     }
@@ -59,7 +59,7 @@ class AuthController extends Controller
     {
         return User::create([
             'name' => $data['name'],
-            'email' => $data['email'],
+            'account' => $data['account'],
             'password' => bcrypt($data['password']),
         ]);
     }
