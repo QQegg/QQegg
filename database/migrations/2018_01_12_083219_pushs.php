30114f47d08c66076e7ba8927660dcf6e5632ac9
陳美妤 <s3a432090@student.ncut.edu.tw>
2018-03-08 16:02:24 +0800

    衝突

--- database/migrations/2018_01_12_083219_pushs.php
+++ database/migrations/2018_01_12_083219_pushs.php
@@ -22,16 +22,7 @@ class Pushs extends Migration
             $table->string('picture');
 //            $table->binary('P_picture')->nullable();
             $table->dateTime('datetime');
-<<<<<<< HEAD
-=======
-            $table->integer('S_id');
-            $table->integer('Cate_id');
-            $table->string('P_title');
-            $table->string('P_content');
-            $table->string('P_picture');
-//          $table->binary('P_picture')->nullable();
-            $table->dateTime('P_timestamp');
->>>>>>> 7202721f6913e2b9b76d30a92c50a07db4d56a14
+
             $table->timestamps();
             $table->rememberToken();
         });
