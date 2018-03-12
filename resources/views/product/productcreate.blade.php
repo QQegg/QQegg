af30139cb77333943e8eb8cc8c69db834d07b803
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2018-03-12 21:29:11 +0800

    修改新增產品的view

--- resources/views/product/productcreate.blade.php
+++ resources/views/product/productcreate.blade.php
@@ -29,38 +29,20 @@
                 <input name="name" class="form-control" placeholder="請輸入產品名稱">
             </div>
             <div class="form-group">
-                <label>選擇產品類別</label>
-                <select name="Category_id" class="form-control">
-                    <option value="1">女生服飾</option>
-                    <option value="2">男生服飾</option>
-                    <option value="3">美妝保健</option>
-                    <option value="4">手機平板與週邊</option>
-                    <option value="5">嬰幼童與母親</option>
-                    <option value="6">3C相關</option>
-                    <option value="7">居家生活</option>
-                    <option value="8">家電影音</option>
-                    <option value="9">戶外與運動用品</option>
-                    <option value="10">美食、伴手禮</option>
-                    <option value="11">汽機車零件百貨</option>
-                    <option value="12">寵物</option>
-                    <option value="13">娛樂</option>
-                </select>
+                <label>產品類別</label>
+                <input name="C_name" class="form-control" placeholder="請輸入產品類別">
             </div>
             <div class="form-group">
                 <label>產品規格</label>
-                <textarea name="specification" class="form-control" rows="5"></textarea>
+                <textarea name="specification" class="form-control" rows="5" placeholder="請輸入產品規格"></textarea>
             </div>
             <div class="form-group">
                 <label>單價</label>
-                <textarea name="price" class="form-control" rows="1"></textarea>
+                <textarea name="price" class="form-control" rows="1" placeholder="請輸入產品單價"></textarea>
             </div>
             <div class="form-group">
                 <label>單位</label>
-                <textarea name="unit" class="form-control" rows="1"></textarea>
-            </div>
-            <div class="form-group">
-                <label>庫存量</label>
-                <textarea name="inventory" class="form-control" rows="1"></textarea>
+                <textarea name="unit" class="form-control" rows="1" placeholder="請輸入產品單位"></textarea>
             </div>
             <div class="form-group">
                 <label>上傳產品照片</label>
