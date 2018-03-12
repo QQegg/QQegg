c5adb4b1b8149ef2345a06226e5feb7e5e8127bf
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2018-03-12 21:27:57 +0800

    修改新增產品的view

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -1,61 +1,70 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Store;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use App\Category;
 use App\Product;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use PhpParser\Node\Scalar\String_;
 
 class ProductsController extends Controller
 {
     public function index()
     {
-        $store = Store::all()->where('email',Auth::guard('store')->user()->email)->pluck('id');
+        $store = Store::all()->where('email', Auth::guard('store')->user()->email)->pluck('id');
 
-        $product=Product::all()->where('store_id', $store['0']);
+        $product = Product::all()->where('store_id', $store['0']);
 
-        return view('product.productlist',compact('product'));
+        return view('product.productlist', compact('product'));
     }
+
     public function create()
     {
         return view('product.productcreate');
     }
 
 
-
     public function store(Request $request)
     {
 
         $messsages = array(
-            'name.required'=>'你必須輸入產品名稱',
-            'specification.required'=>'你必須輸入產品規格',
-            'price.required'=>'你必須輸入單價',
-            'unit.required'=>'你必須輸入單位',
-            'inventory.required'=>'你必須輸入庫存量',
-            'picture.required'=>'你必須選擇照片',
+            'name.required' => '你必須輸入產品名稱',
+            'specification.required' => '你必須輸入產品規格',
+            'price.required' => '你必須輸入單價',
+            'unit.required' => '你必須輸入單位',
+            'picture.required' => '你必須選擇照片',
         );
         $rules = array(
             'name' => 'required|max:255',
-            'Category_id' => 'required',
             'specification' => 'required',
             'price' => 'required|integer',
             'unit' => 'required|string',
-            'inventory' => 'required|integer',
             'picture' => 'required',
         );
 
-        $validator = Validator::make($request->all(), $rules,$messsages);
+        $validator = Validator::make($request->all(), $rules, $messsages);
 
-        if ($validator->fails())
-        {
+        if ($validator->fails()) {
             return redirect()->back()->withErrors($validator->errors());
         }
 
-        $store = Store::all()->where('email',Auth::guard('store')->user()->email)->pluck('id');
+        $store = Store::all()->where('email', Auth::guard('store')->user()->email)->pluck('id');
 
+        $category_name = Category::all()->where('name', $request['C_name']);
+
+        if (count($category_name) == 0) {
+            Category::create([
+                'store_id' => $store['0'],
+                'name' => $request['C_name'],
+            ]);
+        }
+
+        $category_id = Category::all()->where('name', $request['C_name'])->pluck('id');
 
         if ($request->hasFile('picture')) {
             $file_name = $request->file('picture')->getClientOriginalName();
@@ -65,7 +74,7 @@ class ProductsController extends Controller
             // $product->update(['picture' => $file_name]);
 
             Product::create([
-                'Category_id' => $request['Category_id'],
+                'Category_id' => $category_id['0'],
                 'store_id' => $store['0'],
                 'name' => $request['name'],
                 'specification' => $request['specification'],
@@ -77,20 +86,22 @@ class ProductsController extends Controller
         }
         return redirect()->route('procreate');
     }
+
     public function edit($id)
     {
-        $product=Product::all()->where('id',$id);
-        return view('product.productedit',compact('product'));
+        $product = Product::all()->where('id', $id);
+        return view('product.productedit', compact('product'));
     }
-    public function update(Request $request,$id)
+
+    public function update(Request $request, $id)
     {
         $messsages = array(
-            'name.required'=>'你必須輸入產品名稱',
-            'specification.required'=>'你必須輸入產品規格',
-            'price.required'=>'你必須輸入單價',
-            'unit.required'=>'你必須輸入單位',
-            'inventory.required'=>'你必須輸入庫存量',
-            'picture.required'=>'你必須選擇照片',
+            'name.required' => '你必須輸入產品名稱',
+            'specification.required' => '你必須輸入產品規格',
+            'price.required' => '你必須輸入單價',
+            'unit.required' => '你必須輸入單位',
+            'inventory.required' => '你必須輸入庫存量',
+            'picture.required' => '你必須選擇照片',
         );
 
         $rules = array(
@@ -103,19 +114,18 @@ class ProductsController extends Controller
             'picture' => 'required',
         );
 
-        $validator = Validator::make($request->all(), $rules,$messsages);
+        $validator = Validator::make($request->all(), $rules, $messsages);
 
-        if ($validator->fails())
-        {
+        if ($validator->fails()) {
             return redirect()->back()->withErrors($validator->errors());
         }
-        $product=Product::find($id);
+        $product = Product::find($id);
         $product->update($request->all());
-        if ($request->hasFile('picture')){
+        if ($request->hasFile('picture')) {
             $file_name = $request->file('picture')->getClientOriginalName();
 
             $destinationPath = '/public/product';
-            $request->file('picture')->storeAs($destinationPath,$file_name);
+            $request->file('picture')->storeAs($destinationPath, $file_name);
 
             // save new image $file_name to database
             $product->update(['picture' => $file_name]);
@@ -123,9 +133,10 @@ class ProductsController extends Controller
 
         return redirect()->route('prolist');
     }
+
     public function destroy($id)
     {
-        $whereArray = array('id'=>$id);
+        $whereArray = array('id' => $id);
         DB::table('commoditys')->where($whereArray)->delete();
         return redirect()->route('prolist');
     }
