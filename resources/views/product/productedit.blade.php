93f29628adc39942b17d93ea15f19bc24ac5ab0c
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2018-03-13 21:48:20 +0800

    修改產品修改view

--- resources/views/product/productedit.blade.php
+++ resources/views/product/productedit.blade.php
@@ -27,45 +27,27 @@
             {{ method_field('PATCH') }}
             <div class="form-group">
                 <label>產品名稱</label>
-                <input name="name" class="form-control" placeholder=""value="{{$product->name}}">
+                <input name="name" class="form-control" placeholder="請輸入產品名稱"value="{{$product->name}}">
             </div>
             <div class="form-group">
-                <label>選擇產品類別</label>
-                <select name="Category_id">
-                    <option value="1">女生服飾</option>
-                    <option value="2">男生服飾</option>
-                    <option value="3">美妝保健</option>
-                    <option value="4">手機平板與週邊</option>
-                    <option value="5">嬰幼童與母親</option>
-                    <option value="6">3C相關</option>
-                    <option value="7">居家生活</option>
-                    <option value="8">家電影音</option>
-                    <option value="9">戶外與運動用品</option>
-                    <option value="10">美食、伴手禮</option>
-                    <option value="11">汽機車零件百貨</option>
-                    <option value="12">寵物</option>
-                    <option value="13">娛樂</option>
-                </select>
+                <label>產品類別</label>
+                <input name="C_name" class="form-control" placeholder="請輸入產品類別"value="{{$product->C_name}}">
             </div>
             <div class="form-group">
                 <label>產品規格</label>
-                <textarea name="specification" class="form-control" rows="5">{{$product->specification}}</textarea>
+                <textarea name="specification" class="form-control" placeholder="請輸入產品規格" rows="5">{{$product->specification}}</textarea>
             </div>
             <div class="form-group">
                 <label>單價</label>
-                <textarea name="price" class="form-control" rows="1">{{$product->price}}</textarea>
+                <textarea name="price" class="form-control" placeholder="請輸入產品單價" rows="1">{{$product->price}}</textarea>
             </div>
             <div class="form-group">
                 <label>單位</label>
-                <textarea name="unit" class="form-control" rows="1">{{$product->unit}}</textarea>
-            </div>
-            <div class="form-group">
-                <label>庫存量</label>
-                <textarea name="inventory" class="form-control" rows="1">{{$product->inventory}}</textarea>
+                <textarea name="unit" class="form-control" placeholder="請輸入產品單位" rows="1">{{$product->unit}}</textarea>
             </div>
             <div class="form-group">
                 <label>上傳產品照片</label>
-                <input type="file" class="form-control" name="picture" placeholder="上傳圖片">
+                <input type="file" class="form-control" name="picture" placeholder="上傳圖片"{{$product->picture}}>
             </div>
             <div class="btn-group" role="group" aria-label="Basic example">
                 <button type="submit" class="btn btn-success">修改</button>
