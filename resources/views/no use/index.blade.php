36c6c6c4273391cc07be0ae1626a1141f67b3be1
陳欣妤 <lovefish0416@gmail.com>
2018-03-23 15:17:43 +0800

    修改主頁葉面連結

--- resources/views/no use/index.blade.php
+++ resources/views/no use/index.blade.php
@@ -73,8 +73,6 @@
 							<li><a href="{{route('pushlist')}}">促銷訊息管理</a></li>
 							<li><a href="{{route('coulist')}}">折價券</a></li>
 							<li><a href="#">交易紀錄</a></li>
-							<li><a href="{{route('store_change_profile')}}">修改基本資料</a></li>
-							<li><a href="{{route('store_change_password')}}">修改密碼</a></li>
 							<li><a href="{{route('store.login')}}">店家登入</a></li>
 						</ul>
 					</div>
