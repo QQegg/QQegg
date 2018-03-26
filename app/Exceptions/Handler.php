14bb90b0ddb999a59333f8c66d64dabaea7111e8
陳欣妤 <lovefish0416@gmail.com>
2018-03-26 16:36:36 +0800

    判斷是哪個身分者登入

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -67,6 +67,9 @@ class Handler extends ExceptionHandler
             case 'store':
                 $login = 'store.login';
                 break;
+            case 'admin':
+                $login = 'admin.login';
+                break;
             default:
                 $login = 'login';
                 break;
