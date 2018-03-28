ed327bf71e47767d1a384cd9075a37a2a34e7837
陳欣妤 <lovefish0416@gmail.com>
2018-03-28 17:58:18 +0800

    修改管理者修改狀態後的提示

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -37,13 +37,15 @@ class AdminController extends Controller
             case '0':
                 $fix->right=true;
                 $fix->save();
+                return back()->with('success','您已開啟使用權 !');
                 break;
             default:
                 $fix->right=false;
                 $fix->save();
+                return back()->with('error','您已關閉使用權 !');
                 break;
         }
         $accounts = Store::all()->where('id',$id);
-        return back()->with('success','修改成功',compact('accounts'));
+        return view('admin.admin-store',compact('accounts'));
     }
 }
