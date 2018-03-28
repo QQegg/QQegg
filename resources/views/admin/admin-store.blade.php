a802a285b2b31bd7a437a333939af9d0bb5ffb9f
陳欣妤 <lovefish0416@gmail.com>
2018-03-28 17:14:57 +0800

    更改管理店家帳號頁面

--- resources/views/admin/admin-store.blade.php
+++ resources/views/admin/admin-store.blade.php
@@ -12,42 +12,39 @@
                 <div class="panel panel-default">
                     <div class="panel-heading">{{ Auth::guard('admin')->user()->account}}管理者，您正在管理頁面</div>
                     <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0" >
-
                         <thead>
                         <tr>
                             <th width="30" style="text-align: center">id</th>
-                            <th>店家名稱</th>
-                            <th>聯絡人</th>
-                            <th>信箱</th>
-                            <th>電話</th>
-                            <th>地址</th>
-                            <th>圖片</th>
-                            <th>使用權狀態</th>
-                            <th width="100" style="text-align: center">關閉使用權</th>
+                            <th style="text-align: center">店家名稱</th>
+                            <th style="text-align: center">聯絡人</th>
+                            <th style="text-align: center">信箱</th>
+                            <th style="text-align: center">電話</th>
+                            <th style="text-align: center">地址</th>
+                            <th style="text-align: center">圖片</th>
+                            <th style="text-align: center">使用權狀態</th>
+                            <th width="100" style="text-align: center">開啟/停權</th>
                         </tr>
                         </thead>
                         <tbody>
                         @foreach($accounts as $account)
                             <tr>
-                                <td>{{$account->id}}</td>
-                                <td>{{$account->name}}</td>
-                                <td>{{$account->contact}}</td>
-                                <td>{{$account->email}}</td>
-                                <td>{{$account->phone}}</td>
-                                <td>{{$account->address}}</td>
-                                <td>{{($account->picture)?'已上傳':'尚未上傳'}}</td>
-                                <td>{{($account->right)?'開啟':'停權'}}</td>
+                                <td align="center">{{$account->id}}</td>
+                                <td align="center">{{$account->name}}</td>
+                                <td align="center">{{$account->contact}}</td>
+                                <td align="center">{{$account->email}}</td>
+                                <td align="center">{{$account->phone}}</td>
+                                <td align="center">{{$account->address}}</td>
+                                <td align="center">{{($account->picture)?'已上傳':'尚未上傳'}}</td>
+                                <td align="center">{{($account->right)?'正常使用中':'停權'}}</td>
                                 <td>
                                     <form action="{{ route('admin.status', $account->id) }}" method="POST">
-                                        <a href ="{{route('admin.status', ['id'=>$account->id])}}"class="btn btn-success " role="button">開啟</a>
+                                        <a href ="{{route('admin.status', ['id'=>$account->id])}}"class="btn btn-success " role="button" >{{($account->right)?'停權':'開啟'}}</a>
                                         {{ csrf_field() }}
-                                        <button class="btn btn-danger ">停權</button>
                                     </form>
                                 </td>
                             </tr>
                         @endforeach
                         </tbody>
-
                     </table>
                 </div>
 
