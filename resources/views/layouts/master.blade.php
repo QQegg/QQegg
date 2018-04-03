eb70f006cfd90d827057759a58002de9f74bcab5
3A432053許漢昌 <s3a432053@student.ncut.edu.tw>
2018-04-03 20:12:18 +0800

    修改繼承view上的文字項目

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -37,12 +37,7 @@
                 <div class="row">
                     <div class="col-md-6">
                         <ul class="topleft-info">
-                            <a  href="index.html" align="top">
-                                <nobr>
-                                <img src="img/logo2.png" alt="" width="150" height="150" style="float:left;margin:5pt"  />
-                                <h1 style="position: absolute; left:120pt ;top:25pt">商圈促銷資訊就近推播系統</h1>
-                                </nobr>
-                            </a>
+
                         </ul>
                     </div>
                 </div>
@@ -52,22 +47,41 @@
         <div class="navbar navbar-default navbar-static-top">
             <div class="container">
                 <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+                    <a  href="{{route('index')}}" align="top">
+                        <nobr>
+                            <img src="img/logo2.png" alt="" width="150" height="150" style="float:left;margin:5pt"  />
+                            <h1 style="position: absolute; left:400pt ;top:20pt">資訊推播商圈</h1>
+                        </nobr>
+                    </a>
                 </div>
+                @if(Auth::guard('store')->check())
                 <div class="navbar-collapse collapse ">
                     <ul class="nav navbar-nav">
-                        <li><a href="{{route('prolist')}}">商品管理</a></li>
+                        <li font-size="15"><a href="{{route('prolist')}}">商品管理</a></li>
                         <li><a href="{{route('pushlist')}}">促銷訊息管理</a></li>
                         <li><a href="{{route('coulist')}}">折價券</a></li>
                         <li><a href="#">交易紀錄</a></li>
-                        <li><a href="{{route('store.login')}}">店家登入</a></li>
-                        <li><a href="{{route('admin.login')}}">管理者登入</a></li>
+                        <li><a href="{{route('store_change_password')}}">修改密碼</a></li>
+                        <li><a href="{{route('store_change_profile')}}">修改基本資料</a></li>
+                        <li><a href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                            登出
+                        </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
+                        </li>
                     </ul>
                 </div>
+                @else
+                    <div class="navbar-collapse collapse ">
+                        <ul class="nav navbar-nav">
+                            <li><a href="{{route('store.login')}}">店家登入</a></li>
+                            <li><a href="{{route('admin.login')}}">管理者登入</a></li>
+                        </ul>
+                    </div>
+                @endif
             </div>
         </div>
     </header>
