2fb6359510c143a4766c6f0c61093ce0ef081a04
SKILL573 <a9101021249@gmail.com>
2018-04-09 18:51:57 +0800

    結帳暫定

--- resources/views/sale/endprice.blade.php
+++ resources/views/sale/endprice.blade.php
@@ -4,4 +4,5 @@
     <div class="col-md-12">
         <h1> 結帳總金額:{{$price}}</h1>
     </div>
+    <a href="{{route('salecreat')}}"><button type="submit" class="btn btn-success">結帳</button></a>
 @endsection
